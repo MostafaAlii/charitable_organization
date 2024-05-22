4cf837d1ae9edbdcc82b6888f71cc108848eb0c9
Mostafa0Alii <mostafa0alii@gmail.com>
2024-05-22 15:36:46 +0300

    first commit

---
+++ routes/auth.php
@@ -0,0 +1,22 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\{Employee,Admin};
+// Admins ::
+Route::middleware('guest:admin')->prefix('admin')->group(function () {
+    Route::get('login', [Admin\AdminAuthenticatedSessionController::class, 'create'])->name('admin.login');
+    Route::post('login', [Admin\AdminAuthenticatedSessionController::class, 'store']);
+});
+
+Route::middleware('auth:admin')->prefix('admin')->group(function () {
+    Route::post('logout', [Admin\AdminAuthenticatedSessionController::class, 'destroy'])->name('admin.logout');
+});
+
+// Web Employee ::
+/*Route::middleware('guest:web')->prefix('employee')->group(function () {
+    Route::get('login', [Employee\EmployeeAuthenticatedSessionController::class, 'create'])->name('employee.login');
+    Route::post('login', [Employee\EmployeeAuthenticatedSessionController::class, 'store']);
+});
+
+Route::middleware('auth:web')->prefix('employee')->group(function () {
+    Route::post('logout', [Employee\EmployeeAuthenticatedSessionController::class, 'destroy'])->name('employee.logout');
+});*/
---
+++ routes/dashboard/dashboard.php
@@ -0,0 +1,6 @@
+<?php
+use App\Http\Controllers\Dashboard\Admin;
+use Illuminate\Support\Facades\Route;
+Route::group(['prefix' => 'admin','middleware' => 'auth:admin'], function () {
+    Route::get('dashboard', [Admin\DashboardController::class, 'index'])->name('admin.dashboard');
+});
